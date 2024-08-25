7a9b469fecacb398ef0099d0d4037cbc85c65f75
Elena-2019 <loy-iver@inbox.ru>
2024-08-25 16:16:11 +0300

    Update module_2_5_A function with a parameter.py

--- module_2_5_A function with a parameter.py
+++ module_2_5_A function with a parameter.py
@@ -6,23 +6,5 @@ def get_matrix (n, m, value):
             matrix[i].append(value)
     print(matrix)
 get_matrix(2, 2, 10)
-
-
-def get_matrix (n, m, value):
-    matrix = []
-    for i in range(n):
-        matrix.append([])
-        for j in range(m):
-            matrix[i].append(value)
-    print(matrix)
 get_matrix(3, 5, 42)
-
-
-def get_matrix (n, m, value):
-    matrix = []
-    for i in range(n):
-        matrix.append([])
-        for j in range(m):
-            matrix[i].append(value)
-    print(matrix)
 get_matrix(4, 2, 13)
